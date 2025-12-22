c62e03894cc9276db22f982c9a2168bf38dcdba0
Atari Fan <167288874+Issac567@users.noreply.github.com>
2025-12-22 13:16:42 -0800

    Add files via upload
    File structure changed

---
+++ Source Code SFX/www/JavaScript/init.js
@@ -0,0 +1,12 @@
+//-------------------------------------------------------------------------------------------------------------------
+// Initial entry in script.  Must be added in last in script
+//-------------------------------------------------------------------------------------------------------------------
+
+// One time Setup call 
+setupStepPanelInputsListener();
+
+// One time Setup call 
+setupKeyboardButtonListener();
+
+// Load default screech shoot json on startup
+loadJSONScreechShootTable();
