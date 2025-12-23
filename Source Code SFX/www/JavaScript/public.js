4e9f30ca9f642265c3ecb7151fecde7d3d04ed3d
Atari Fan <167288874+Issac567@users.noreply.github.com>
2025-12-23 15:31:09 -0800

    Set DEBUG to false for release

--- Source Code SFX/www/JavaScript/public.js
+++ Source Code SFX/www/JavaScript/public.js
@@ -1,4 +1,4 @@
-const DEBUG = true;                 // ← set false for release
+const DEBUG = false;                 // ← set false for release
 
 let tonesArray = [];
 let toneStopTime = 0                // to track stop time (Off duration: Silence Gap)
@@ -33,4 +33,5 @@ function loadJSONScreechShootTable() {
 
     tonesArray = JSON.parse(jsonData);
     updateTable();
+
 }
